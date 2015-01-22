020f690c7d535b74ca9532014670a9e263deaadd
sagarmpatel2161992 <sagarmpatel2161992@yahoo.com>
2015-01-22 12:42:59 -0800

    Hero and main method created

---
+++ src/Game.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+
+/**
+ * @author SAGAR
+ *
+ */
+public class Game {
+
+	/**
+	 * 
+	 */
+	public Game() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Hero sagar = new Hero("Saggy");
+		sagar.show();
+		sagar.fight();
+		sagar.show();
+
+	}
+
+}
---
+++ src/Hero.java
@@ -0,0 +1,15 @@
+
+public class Hero {
+	
+	private int strength;
+	private int speed;
+	private int health;
+	String name;
+	public Hero(String name) {
+		super();
+		this.name = name;
+		generateAbility();
+	}
+	
+	 		
+}
