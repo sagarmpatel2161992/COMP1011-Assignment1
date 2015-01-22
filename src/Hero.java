a2f782e0f798a349321a067bba7524594c3298d3
sagarmpatel2161992 <sagarmpatel2161992@yahoo.com>
2015-01-22 12:46:30 -0800

    hitattempt, hitdamage and show method added

--- src/Hero.java
+++ src/Hero.java
@@ -28,5 +28,33 @@ public class Hero {
 		
 	 }
 	 
+
+	 public boolean hitAttempt()
+	 {
+		 int hitrandom = (int) ((Math.random()*100 +1)%101);
+		 
+		 if(hitrandom <= 20)
+		 {
+			 return true;
+		 }
+		 
+		 return false;
+	 }
+	 
+	 public int hitDamage()
+	 {
+		 int factor =  (int) ((Math.random()*100 +1)%7);
+		 return (this.strength * factor);
+	 }
+	 
+	 public void show()
+	 {
+		System.out.println("\nAbility Of " + this.name);
+		System.out.println("------------------------------------------------");
+		System.out.println("Strength Of " + this.name + " is "+ this.strength);
+		System.out.println("Speed Of " + this.name + " is "+ this.strength);
+		System.out.println("Health Of " + this.name + " is "+ this.strength);		
+	 }
+	 
 	 		
 }
